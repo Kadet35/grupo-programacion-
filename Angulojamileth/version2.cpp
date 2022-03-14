fb3b79c6663b4798e440ae4e6067b5957511c045
Kadet35 <kevin.bustos.reyes@utelvt.edu.ec>
2022-03-13 21:27:09 -0500

    version 2 de la suma

---
+++ Angulojamileth/version2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        float A,B,C
+        cout<<“ingrese el 2 numeros A y B;";
+        cin>>A>>B;
+        c=A+B;
+        cout<<“El resultado de la suma es:";
+        cout<<C;
+        return 0;
+}
