ac0024dd195fe81c11c34d7fb3578f509cbda237
3RCK <erickmanzaba@hotmail.com>
2022-04-23 19:46:40 -0500

    Compara.cpp

---
+++ ManzabaErick/ManzabaErick-Compara.cpp
@@ -0,0 +1,21 @@
+#include<iostrea>
+using namespace std;
+int main()
+{
+	float x,y;
+	cout<<"Ingrese la x: "cin>>x;
+	cout<<"Ingrese la y: "cin>>y;
+
+	if(x==y){
+		cout<<"El valor de x= "<<x<<"es igual a y= "<<y<<endl;
+	}else{
+		if(x==Y){
+			cout<<"El valor de x= "<<x<<"es menor a y= "<<y<<endl;
+		}else{
+			cout<<"El valor de y= "<<y<<"es menor a x= "<<x<<endl;
+		}
+	}
+	return 0;
+
+
+}
