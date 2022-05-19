1fb7d8f343c468335b14115849bf498dbb686155
3RCK <erickmanzaba@hotmail.com>
2022-05-19 15:52:41 -0500

    Actividad-C2

--- ManzabaErick/ACTIVIDAD-B2-C2/ManzabaErick-CuentaMoneda.cpp
+++ ManzabaErick/ACTIVIDAD-B2-C2/ManzabaErick-CuentaMoneda.cpp
@@ -1,33 +1,39 @@
+//================================================
+//==> Nombre del programa: Cuenta monedas
+//==> Archivo : CuentaMoneda.cpp
+//==>Autor: Erick Manzaba
+//==>Fecha de elaboración: 2022-04-30
+//==>Fecha ultima actualización: 2022-05-19
+//=================================================
 #include<iostream>
 using namespace std;
 int main()
 {
 
-	int n,c=0,c1=0,c2=0;
-	float x,a=0,a1=0,a2=0,m=0.10;
+	int em_n,em_c=0,em_c1=0,em_c2=0;
+	float em_x,em_a=0,em_a1=0,em_a2=0,em_m=0.10;
 	cout<<"Cantidad de monedas a ingresar: ";
-	cin>>n;
+	cin>>em_n;
 	do{
 		cout<<"Ingrese el valor de la mondea (0.10,0.25): ";
-		cin>>x;
-		c=c+1;
-		a=a+x;
+		cin>>em_x;
+		em_c=em_c+1;
+		em_a=em_a+em_x;
 		if(x==m){
-			c1=c1+1;
-			a1=a1+x;
+			em_c1=em_c1+1;
+			em_a1=em_a1+x;
 		}else{
-			c2=c2+1;
-			a2=a2+x;
+			em_c2=em_c2+1;
+			em_a2=em_a2+em_x;
 		}
-	}while(c>m);
+	}while(em_c>em_m);
 	cout<<"El resultado fue: "<<endl;
-	cout<<"Cantidad de monedas ingresadas: "<<c<<endl;
-	cout<<"Cantidad de total en dinero contado: "<<a<<endl;
-	cout<<"Cantidad de monedas de 0.10cc ingresadas: "<<c1<<endl;
-	cout<<"Cantidad total de monedas de 0.10cc: "<<a1<<endl;
-	cout<<"Cantidad de monedas de 0.25cc ingresadas: "<<c2<<endl;
-	cout<<"Cantidad total en dinero de monedas de 0.25cc: "<<a2<<endl;
+	cout<<"Cantidad de monedas ingresadas: "<<em_c<<endl;
+	cout<<"Cantidad de total en dinero contado: "<<em_a<<endl;
+	cout<<"Cantidad de monedas de 0.10cc ingresadas: "<<em_c1<<endl;
+	cout<<"Cantidad total de monedas de 0.10cc: "<<em_a1<<endl;
+	cout<<"Cantidad de monedas de 0.25cc ingresadas: "<<em_c2<<endl;
+	cout<<"Cantidad total en dinero de monedas de 0.25cc: "<<em_a2<<endl;
 
 	return 0;
 }
-
