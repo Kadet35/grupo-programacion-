327d7558163423ccddc6957e3820b25be97a7f52
3RCK <erickmanzaba@hotmail.com>
2022-05-19 16:00:43 -0500

    Actividad-C2

--- ManzabaErick/ACTIVIDAD-B2-C2/ManzabaErick-SumaN.cpp
+++ ManzabaErick/ACTIVIDAD-B2-C2/ManzabaErick-SumaN.cpp
@@ -1,12 +1,20 @@
+//================================================
+//==> Nombre del programa: Suma de dos numeros
+//==> Archivo : SumaN.cpp
+//==>Autor: Erick Manzaba 
+//==>Fecha de elaboración: 2022-04-30
+//==>Fecha ultima actualización: 2022-05-19
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
 
-	float b,d,S;
-	cin>>b>>d;
-	S=b+d;
-	cout<<"El valor de la suma es: "<<S;
+	float em_b,em_d,em_S;
+	cin>>em_b>>em_d;
+	em_S=em_b+em_d;
+	cout<<"El valor de la suma es: "<<em_S;
 
 	return 0;
 }
