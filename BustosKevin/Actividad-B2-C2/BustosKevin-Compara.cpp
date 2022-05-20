f47e4303979aaca253fead493623271504f71327
Kadet35 <kevin.bustos.reyes@utelvt.edu.ec>
2022-05-20 16:03:26 -0500

    edit

--- BustosKevin/Actividad-B2-C2/BustosKevin-Compara.cpp
+++ BustosKevin/Actividad-B2-C2/BustosKevin-Compara.cpp
@@ -1,12 +1,14 @@
 //================================================
-////==> Nombre del programa: compara dos numeros editado
-////==> Archivo : compara.cpp
-////==>Autor: Kevin Bustos
-////==>Fecha de elaboración: 2022-05-20
-////==>Fecha ultima actualización: 2022-05-120
-////================================================
+//==> Nombre del programa: compara dos numeros editado
+//==> Archivo : compara.cpp
+//==>Autor: Kevin Bustos
+//==>Fecha de elaboración: 2022-05-20
+//==>Fecha ultima actualización: 2022-05-120
+//=================================================
+
 #include<iostream>
 using namespace std;
+
 int main()
 {
 float kb_a, kb_b; 
@@ -33,5 +35,5 @@ cout<<"//==>Autor: Kevin Bustos"<<endl;
 cout<<"//==>Fecha de elaboracion: 2022-05-20"<<endl;
 cout<<"//==>Fecha ultima actualización: 2022-05-120"<<endl;
 cout<<"//================================================="<<endl;
- return 0;
- }
+  return 0;
+}
