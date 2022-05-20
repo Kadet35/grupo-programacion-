aa259550d6b5cd23ed71192b24b49d4f2c8ceef7
Kadet35 <kevin.bustos.reyes@utelvt.edu.ec>
2022-05-20 16:35:12 -0500

    edad

--- BustosKevin/Actividad-B2-C2/BustosKevin-leaedad.cpp
+++ BustosKevin/Actividad-B2-C2/BustosKevin-leaedad.cpp
@@ -1,25 +1,40 @@
+//================================================
+////==> Nombre del programa: leaedad
+////==> Archivo : BustosKevin-leaedad.cpp
+////==>Autor: Kevin Bustos
+////==>Fecha de elaboración: 2022-05-20
+////==>Fecha ultima actualización: 2022-05-120
+////=================================================
+//
 #include<iostream>
 using namespace std;
 int main()
 {
-int d,m,a,d1,m1,a1,d2,m2,a2;
+int kb_d,kb_m,kb_a,kb_d1,kb_m1,kb_a1,kb_d2,kb_m2,kb_a2;
 cout<<"ingrese fecha actual:  dia mes año";
-cin>>d>>m>>a;
+cin>>kb_d>>kb_m>>kb_a;
 cout<<"ingre su fecha de nacimiento: dia mes año";
-cin>>d1>>m1>>a1;
-if(d<d1)
-{ d=d+30;
-  m=m-1;
-  d2=d-d1;
-}
-else{ d2=d-d1;}
-if(m<m1) {
-m=m+12;
-a=a-1;
-m2=m-m1;
-}
-else{ m2=m-m1; }
-a2=a-a1;
-cout<<"Usted tiene: "<<a2<<" años "<<m2<<"meses "<<"y "<<d2<<"dia "<<endl;
-return 0;
-}
+cin>>kb_d1>>kb_m1>>kb_a1;
+if(kb_d<kb_d1)
+{ kb_d=kb_d+30;
+kb_m=kb_m-1;
+kb_d2=kb_d-kb_d1;
+   }
+   else{ kb_d2=kb_d-kb_d1;}
+   if(kb_m<kb_m1) {
+    kb_m=kb_m+12;
+    kb_a=kb_a-1;
+   kb_m2=kb_m-kb_m1;
+   }
+    else{ kb_m2=kb_m-kb_m1; }
+    kb_a2=kb_a-kb_a1;
+    cout<<"Usted tiene: "<<kb_a2<<" años "<<kb_m2<<"meses "<<"y "<<kb_d2<<"dia "<<endl;
+   cout<<endl<<"//================================================"<<endl;
+   cout<<"//==> Nombre del programa: leaedad"<<endl;
+   cout<<"//==> Archivo : BustosKevin-leaedad.cpp"<<endl;
+   cout<<"//==>Autor: Kevin Bustos"<<endl;
+    cout<<"//==>Fecha de elaboración: 2022-05-20"<<endl;
+   cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+  cout<<"//================================================="<<endl;
+     return 0;
+    }
