718fb93a881416333130d354572ff1f8fd781c96
Kadet35 <kevin.bustos.reyes@utelvt.edu.ec>
2022-05-20 16:24:51 -0500

    suma

--- BustosKevin/Actividad-B2-C2/BustosKevin-Suma.cpp
+++ BustosKevin/Actividad-B2-C2/BustosKevin-Suma.cpp
@@ -1,18 +1,34 @@
+//================================================
+//==> Nombre del programa: Suma
+//==> Archivo : BustosKevin-Suma.cpp
+//==>Autor: Kevin Bustos
+//==>Fecha de elaboración: 2022-05-20
+//==>Fecha ultima actualización: 2022-05-120
+//=================================================
+
 #include<iostream>
 using namespace std;
+
 int main()
 {
-int a,b,c,i;
-	c=c=0;
+int kb_a,kb_b,kb_c,kb_i;
+	kb_c=kb_c=0;
 cout<<"ingres cuantos numeros quiere sumar: \n";
-cin>>a;
-for(i=1;i<=a;i++)
+cin>>kb_a;
+for(kb_i=1;kb_i<=kb_a;kb_i++)
 {
 cout<<"ingre valor:";
-cin>>b;
-c=c+b;
+cin>>kb_b;
+kb_c=kb_c+kb_b;
 }
-cout<<"la suma total es: "<<c;
-return 0;
+cout<<"la suma total es: "<<kb_c;
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Suma"<<endl;
+cout<<"//==> Archivo : BustosKevin-Suma.cpp"<<endl;
+cout<<"//==>Autor: Kevin Bustos"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-05-20"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+  return 0;
 }
 
