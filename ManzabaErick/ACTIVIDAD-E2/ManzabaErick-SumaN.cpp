d8470c2819739b3807ace041b6a3cd57ecf5ecd6
3RCK <erickmanzaba@hotmail.com>
2022-05-27 12:26:26 -0500

    Examen

---
+++ ManzabaErick/ACTIVIDAD-E2/ManzabaErick-SumaN.cpp
@@ -0,0 +1,29 @@
+//================================================
+//==> Nombre del programa: Suma
+//==> Archivo : ManzabaErick-SumaN.cpp
+//==>Autor: Erick Manzaba
+//==>Fecha de elaboración: 2022-04-30
+//==>Fecha ultima actualización: 2022-05-19
+//=================================================
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int em_a,em_b,em_c,em_i;
+	em_c=em_c=0;
+cout<<"ingres cuantos numeros quiere sumar: \n";
+cin>>em_a;
+for(em_i=1;em_i<=em_a;em_i++)
+{
+cout<<"ingre valor:";
+cin>>em_b;
+em_c=em_c+em_b;
+}
+cout<<"la suma total es: "<<em_c;
+cout<<endl<<
+
+  return 0;
+}
+
